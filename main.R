8c295f79f32f6a8c6c2404189ed6697ff313b512
muq33 <muqq33@gmail.com>
2024-01-22 11:35:26 -0300

    update

--- main.R
+++ main.R
@@ -16,6 +16,8 @@ dados$grupo <- factor(dados$grupo)
 
 plot(fitdist(dados$tempo_duracao, distr = "gamma"))
 plot(fitdist(dados$tempo_duracao, distr = "exp"))
+plot(fitdist(dados$tempo_duracao, distr = "invgauss", start = list(mean = 1, shape = 1)))
+ 
 summaryBy(tempo_duracao~grupo, data=dados, FUN=c(mean, sd))
 plotMeans(dados$tempo_duracao,  dados$grupo,  error.bars="se")
 with(dados, plot(densidade_máxima, tempo_duracao))
@@ -23,7 +25,7 @@ with(dados, plot(densidade_máxima, tempo_duracao))
 ggplot(dados, aes(x=densidade_máxima, y=tempo_duracao, color=grupo)) + 
   geom_point(size=6) + ggtitle("Gráfico de dispersão dos dados")
 
-#Modelo
+#Modelo - Gamma
 
 ajust <- vector("list", 3)
 formulas <- c(tempo_duracao~densidade_máxima, tempo_duracao~densidade_máxima + grupo, tempo_duracao~densidade_máxima*grupo)
@@ -32,7 +34,7 @@ ajust[[1]] <- glm(formulas[[1]], data = dados, family = Gamma(link = "identity")
 ajust[[2]] <- glm(formulas[[2]], data = dados, family = Gamma(link = "identity"))
 ajust[[3]] <- glm(formulas[[3]], data = dados, family = Gamma(link = "identity"))
 
-#Testes
+#Testes - Gamma
 
 testes <- vector("list", 2)
 testes[[1]] <- lrtest(ajust[[1]],ajust[[2]])
@@ -51,9 +53,44 @@ saida_teste <- data.frame(
 ); xtable(saida_teste)
 
 
-linearHypothesis(ajust[[3]],hypothesis.matrix=c(0,0,0,0,0,1,1,1))
-
+linearHypothesis(ajust[[3]],hypothesis.matrix=c(0,0,0,0,1,1))
+linearHypothesis(ajust[[3]],hypothesis.matrix=c(0,0,0,0,1,1))
   
-#Diagnóstico
+#Diagnóstico - Gamma
 fit.model <- ajust[[3]]
 source("envelope_gama_ident.R")
+
+
+#Modelo - Normal Inversa
+
+ajust_ninv <- vector("list", 3)
+
+ajust_ninv[[1]] <- glm(formulas[[1]], data = dados, family = inverse.gaussian(link = "identity"))
+ajust_ninv[[2]] <- glm(formulas[[2]], data = dados, family = inverse.gaussian(link = "identity"))
+ajust_ninv[[3]] <- glm(formulas[[3]], data = dados, family = inverse.gaussian(link = "identity"))
+
+#Testes - Normal Inversa
+
+testes_ninv <- vector("list", 2)
+testes_ninv[[1]] <- lrtest(ajust_ninv[[1]],ajust_ninv[[2]])
+testes_ninv[[2]] <- lrtest(ajust_ninv[[2]],ajust_ninv[[3]])
+
+formulas_str <- as.character(formulas)
+saida_teste_ninv <- data.frame(
+  formula = c(paste0(formulas_str[[1]], " : ", formulas_str[[2]]),
+              paste0(formulas_str[[2]], " : ", formulas_str[[3]])),
+  Df = c(testes_ninv[[1]]$Df[[2]],
+         testes_ninv[[2]]$Df[[2]]),
+  Chisq = c(testes_ninv[[1]]$Chisq[[2]],
+            testes_ninv[[2]]$Chisq[[2]]),
+  p = c(testes_ninv[[1]]$`Pr(>Chisq)`[[2]],
+        testes_ninv[[2]]$`Pr(>Chisq)`[[2]])
+); xtable(saida_teste_ninv)
+
+
+linearHypothesis(ajust_ninv[[3]],hypothesis.matrix=c(0,0,0,0,1,1))
+linearHypothesis(ajust_ninv[[3]],hypothesis.matrix=c(0,0,0,0,1,1))
+
+#Diagnóstico - Normal Inversa
+fit.model <- ajust_ninv[[3]]
+source("envelope_ginv_ident.R")
